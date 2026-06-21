359388da0142ab1b4195900f1228dbca20f924ed
fffaheem <mohdfaheemahmad5@gmail.com>
2026-06-21 21:48:32 +0530

    - working modal added

--- script.js
+++ script.js
@@ -4,14 +4,34 @@
     exportBtn : document.getElementById("exportBtn"),
     importBtn : document.getElementById("importBtn"),
     addBtn : document.getElementById("addBtn"),
-    modal : document.getElementById("modal"),
+    modalOut : document.getElementById("modalOut"),
+    modalInner : document.getElementById("modalInner"),
+    closeModal : document.getElementById("closeModal"),
   }
 
-  function addNote(e) {
-    console.log("hello",e.target);
+  function addModal() {
+    Elements.modalOut.classList.add("show")
+    return
   }
 
+  function closeModal(e) {
+    if (e.target !== Elements.closeModal &&
+        e.target !== Elements.modalOut) {
+      return
+    }
+    
+    Elements.modalOut.classList.remove("show")
+    return
+    
+  }
   
-  Elements.addBtn.addEventListener(("click"), addNote);
+  Elements.addBtn.addEventListener(("click"), addModal);
+  Elements.importBtn.addEventListener(("click"), addModal);
+  Elements.exportBtn.addEventListener(("click"), addModal);
+  Elements.deleteBtn.forEach((btns) => {
+    btns.addEventListener(("click"), addModal);
+  })
+  Elements.modalOut.addEventListener(("click"), closeModal);
+  
   
 })();
